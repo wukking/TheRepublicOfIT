e6e28e4fffc45c1ea2af7dbe14f5812ce97e2471
WuYans <wuyson@163.com>
2023-09-15 16:31:12 +0800

    添加跑马灯demo

--- ui/home/src/main/java/com/wuyson/ui/home/HomeMainFragment.kt
+++ ui/home/src/main/java/com/wuyson/ui/home/HomeMainFragment.kt
@@ -5,14 +5,23 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.wuyson.ui.home.databinding.HomeFragmentMainBinding
 
 class HomeMainFragment :Fragment(){
+    private lateinit var binding:HomeFragmentMainBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.home_fragment_main,container,false)
+        binding = HomeFragmentMainBinding.inflate(inflater)
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.tvHome.isSelected = true
     }
 }
