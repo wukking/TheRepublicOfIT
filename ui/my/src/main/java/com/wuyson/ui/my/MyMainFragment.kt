638dab155bc443b1e1d7f80a6af42985642891ea
WuYans <wuyson@163.com>
2023-09-15 16:14:08 +0800

    布局修改

--- ui/my/src/main/java/com/wuyson/ui/my/MyMainFragment.kt
+++ ui/my/src/main/java/com/wuyson/ui/my/MyMainFragment.kt
@@ -5,14 +5,26 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.wuyson.ui.my.databinding.MyFragmentMainBinding
 
 class MyMainFragment : Fragment() {
+    private var _binding:MyFragmentMainBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.my_fragment_main,container,false)
+    ): View {
+        _binding = MyFragmentMainBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.btnGuide.setOnClickListener {
+//            findNavController().navigate()
+        }
     }
 }
